0873fd2517988b1552dd8b0b9dbceb3f6eddfdcd
asheralley <asher.asheralley@gmail.com>
2019-11-17 13:30:54 +1300

    Testing javascript

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-document.addEventListener('scroll', scroller);
+document.addEventListener("scroll", scroller);
 
-function scroller(){
+function scroller() {
   let x = window.scrollY;
-  let y = document.querySelector('ul');
-  let nav = document.querySelector('nav');
+  let y = document.querySelector("ul");
+  let nav = document.querySelector("nav");
   let navPos = window.scrollY;
-
-  if(x == 33){
+  // testing one two!
+  if (x == 33) {
     //y.classList.add('scrolled');
-    console.log('here');
+    console.log("here");
     console.log(nav.getBoundingClientRect());
   } else {
     //y.classList.remove('scrolled');
